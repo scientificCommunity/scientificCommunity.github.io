e2ad7161e4d52f5cec076375b80b6b3873fad6ab
Krivers <river.boat.snow@gmail.com>
2021-12-16 18:19:53 +0800

    update sync script

--- sync_download.sh
+++ sync_download.sh
@@ -1,1 +1,2 @@
-scp tx:~/blog/sync-data/theme-next_config.yml /Users/tk/mywork/blog/hexo/themes/next/_config.yml
+scp tx:~/blog/sync-data/theme-next_config.yml ~/mywork/blog/hexo/themes/next/_config.yml
+scp tx:~/blog/sync-data/db.json ~/mywork/blog/hexo/db.json
--- sync_upload.sh
+++ sync_upload.sh
@@ -1,1 +1,2 @@
-scp /Users/tk/mywork/blog/hexo/themes/next/_config.yml tx:~/blog/sync-data/theme-next_config.yml
+scp ~/mywork/blog/hexo/themes/next/_config.yml tx:~/blog/sync-data/theme-next_config.yml
+scp ~/mywork/blog/hexo/db.json tx:~/blog/sync-data/db.json
